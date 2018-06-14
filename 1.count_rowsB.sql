11596e315480ded082009936c333876e355b7412
Patrick Klostermann <dev@netflair.at>
2018-06-14 11:32:20 +0200

    -

--- 1.count_rowsB.sql
+++
@@ -1,7 +0,0 @@
-SELECT 
-( SELECT COUNT(*) FROM departments ) AS departments_count, 
-( SELECT COUNT(*) FROM dept_emp ) AS dept_emp_count,
-( SELECT COUNT(*) FROM dept_manager) AS dept_manager_count,
-( SELECT COUNT(*) FROM employees) AS employees_count,
-( SELECT COUNT(*) FROM salaries) AS salaries_count,
-( SELECT COUNT(*) FROM titles) AS titles_count
